843c521493178e3814ccc8728449b4df0d7b774b
Dani Akhmad Maulana <121219043+daniAkhmadMaulana@users.noreply.github.com>
2024-08-30 07:02:35 +0700

    Create dasar.js
    Belum selesai

---
+++ dasar.js
@@ -0,0 +1,19 @@
+// Belajar JS
+// dalam JS ada komsep variabel, tipe data, operator, percabangan, perulangan, function, object, array
+// Variabel
+const varKonstan = "Tidak dapat diupdate lagi";
+let varCanUbah = "Bisa di deklarasikan ulang dengan nilai terbaru";
+// Semua peraturan deklarasi variabel dapat diterapkan
+
+// Tipe data
+// string, number, boolean, null, undefined
+let varString = "Kata atau kalimat";
+let varNumber1 = 100; // int atau bilangan bulat
+let varNumber2 = 9.99; // float atau bilangan desimal
+let varBool = true;
+let varNothing = null;
+let varNotDefined = undefined;
+
+// Operator
+
+
